3c5cc2804bac111d5e14907f6e09409498f89257
Nikolay Repin <nikolat-repin201303@mail.ru>
2024-08-10 20:40:35 +0700

    Add code that sets up stack segment in `bootloader.asm`

--- src/bootloader/bootloader.asm
+++ src/bootloader/bootloader.asm
@@ -1,7 +1,21 @@
 rsect bootloader_start
 __bootloader_start_c: ext
 
+MMU_CTX_REG: ext
+MMU_TABLE: ext
+
 start>
+
+	ldi	r0, MMU_CTX_REG
+	ldi	r1, 0
+	stw	r0, r1
+
+	ldi	r0, MMU_TABLE+32
+	ldi	r1, 0x8000
+	stw	r0, r1
+	ldi	r0, MMU_TABLE+96
+	ldi r1, 0xfd00
+	stw r0, r1
 
 	ldi r0, 0x1000
 	stsp r0
